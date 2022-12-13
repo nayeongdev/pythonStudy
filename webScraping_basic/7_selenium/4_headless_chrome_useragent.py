663f7f3f4a839223b89f9f3e59aac5403da4d2f2
nayeongdev <nayeongkim.dev@gmail.com>
2022-12-13 20:31:02 +0900

    haedless Chrome

---
+++ webScraping_basic/7_selenium/4_headless_chrome_useragent.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+options.headless = True
+options.add_argument("window-size=1920x1080")
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
+
+browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                           options=options)
+browser.maximize_window()
+
+url="https://www.whatismybrowser.com/detect/what-is-my-user-agent/"
+browser.get(url)
+
+detected_value=browser.find_element(By.ID,"detected_value")
+print(detected_value.text)
+browser.quit()
